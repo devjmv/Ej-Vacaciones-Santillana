73ceed072c560bf4d16a3c502151d19f5db661dd
devjmv <javier161890@gmail.com>
2024-07-17 21:23:54 +0200

    init spring boot

---
+++ src/main/java/com/vacaciones/santadillas/SantadillasApplication.java
@@ -0,0 +1,13 @@
+package com.vacaciones.santadillas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantadillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantadillasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacaciones/santadillas/SantadillasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacaciones.santadillas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantadillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
