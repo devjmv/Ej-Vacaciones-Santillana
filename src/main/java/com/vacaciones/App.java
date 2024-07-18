00b3edf9c47ae476eb02fdb9db5a23442ddc1776
devjmv <javier161890@gmail.com>
2024-07-18 12:29:52 +0200

    ej amstrong

--- src/main/java/com/vacaciones/App.java
+++ src/main/java/com/vacaciones/App.java
@@ -2,6 +2,7 @@ package com.vacaciones;
 
 import java.util.Scanner;
 
+import com.vacaciones.Models.AmstrongModel;
 import com.vacaciones.Models.MesModel;
 
 /**
@@ -18,12 +19,23 @@ public final class App {
     public static void main(String[] args) {
         // Ej Meses y días
         Scanner entrada = new Scanner(System.in);
-        System.out.print("Introduce el mes numerico: ");
+        System.out.println("Ej Meses y Días");
+        System.out.print("Introduce el mes numerico de 1 al 12: ");
         int mesInt = entrada.nextInt();
         MesModel mes = new MesModel(mesInt);
         String day = mes.GetPropiedades();
         System.out.println(day);
-        entrada.close();
         //
+
+        // Ej Amstrong
+        System.out.println("Ej Amstrong");
+        System.out.print("Introduce el numero: ");
+        int number = entrada.nextInt();
+        AmstrongModel amstrong = new AmstrongModel(number);
+        String isAmstrong = amstrong.ifAmstrong();
+        System.out.println(isAmstrong);
+        //
+
+        entrada.close();
     }
 }
