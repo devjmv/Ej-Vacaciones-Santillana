3756cadc956cadf372298f7f8d16b43d4713af0d
devjmv <javier161890@gmail.com>
2024-08-02 21:43:22 +0200

    add test of metods in model MesModel

--- src/test/java/com/vacaciones/Models/MesModelTest.java
+++ src/test/java/com/vacaciones/Models/MesModelTest.java
@@ -1,15 +1,19 @@
 package com.vacaciones.Models;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
 public class MesModelTest {
     @Test
     void testGetDayOfMonth() {
-
+        MesModel mes = new MesModel(2);
+        assertEquals("Febrero", mes.GetNumberDayOfMonth());
     }
 
     @Test
     void testGetNumberDayOfMonth() {
-
+        MesModel mes = new MesModel(2);
+        assertEquals(29, mes.GetDayOfMonth());
     }
 }
